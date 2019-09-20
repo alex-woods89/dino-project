0569a46c188734ef2c26c6b0522c2db8031b6e4a
Liliana Everett <everettliliana@gmail.com>
2019-09-20 17:36:40 +0100

    add gitignore

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fetch = require('node-fetch');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('coin-data', (req, res) => {
+  const url = 'http://dinosaurpictures.org/api/category/all';
+  fetch(url)
+    .then(jsonData => jsonData.json())
+    .then(data => console.log((data));)
+});
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('dino_favorites');
+    const dinoFavCollection = db.collection('favs');
+    app.use('/api/favs', createRouter(dinoFavCollection));
+  })
+  .catch(console.error);
+
+app.listen(3000, function() {
+  console.log(`Dino server running on port ${this.address().port}`);
+});
