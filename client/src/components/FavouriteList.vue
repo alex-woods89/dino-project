d61c0326b999ff2bffc7fec954413fd02dcabfe4
Alex Woods <alex.woods89@hotmail.co.uk>
2019-09-24 10:10:20 +0100

    updated

--- client/src/components/FavouriteList.vue
+++ client/src/components/FavouriteList.vue
@@ -1,11 +1,13 @@
 <template>
 <div>
+  
   <h3>Your Favourite Dinos</h3>
   <br>
   <br>
   <fav-list-item
   v-for="(favourite, index) in favourites" :favourite="favourite" :key="index">
   </fav-list-item>
+  
 </div>
 </template>
 
