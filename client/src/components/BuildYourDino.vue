06ba0ed342f376a429ec7584b8c9b0b723cc023f
Liliana Everett <everettliliana@gmail.com>
2019-09-24 16:25:15 +0100

    build a dino edited

--- client/src/components/BuildYourDino.vue
+++ client/src/components/BuildYourDino.vue
@@ -9,7 +9,48 @@
       <div class="form-col">
         <input v-model="name" >
       </div>
+          <div class="form-row">
+      <div class="form-col">
+        <label for="dino-color">Dino Colour</label>
+      </div>
+      <div class="form-col">
+        <select class="" v-model="color">
+          <option value="">What colour is your Dino?</option>
+          <option value="Red">Red</option>
+          <option value="Green">Green</option>
+          <option value="Blue">Blue</option>
+          <option value="Yellow">Yellow</option>
+        </select>
+      </div>
+      <h2>Is your Dino bigger than a house?</h2>
+    <label for="dino-house">Yes</label>
+    <input type="radio" id = "yes" name="house" value="bigger" v-model="selected">
+    <label for="dino-house">No</label>
+    <input type="radio" id ="no" name="house" value="smaller" v-model="selected">
     </div>
+        <div class="form-row">
+      <div class="form-col">
+        <label for="dino-live">Dino Home</label>
+      </div>
+      <div class="form-col">
+        <select v-model="live">
+          <option value="">Where does your Dino live?</option>
+          <option value="Cave">Cave</option>
+          <option value="Forest">Forest</option>
+          <option value="Bunker">Bunker</option>
+          <option value="The Moon">The Moon</option>
+        </select>
+      </div>
+          <div class="form-row">
+      <div class="form-col">
+        <h2>Can your Dino fly?</h2>
+      </div>
+          <label for="dino-fly">Yes</label>
+          <input type="radio" id = "yes" name="fly" value="fly" v-model="picked">
+      <label for="dino-fly">No</label>
+      <input type="radio" id ="no" name="fly" value="not fly" v-model="picked">
+    </div>
+    <br>
     <div class="form-row">
       <div class="form-col">
         <label for="dino-food">Dino Food</label>
@@ -24,52 +65,16 @@
         </select>
       </div>
     </div>
-    <div class="form-row">
-      <div class="form-col">
-        <label for="dino-live">Dino Home</label>
-      </div>
-      <div class="form-col">
-        <select v-model="live">
-          <option value="">Where does your Dino live?</option>
-          <option value="Cave">Cave</option>
-          <option value="Forest">Forest</option>
-          <option value="Bunker">Bunker</option>
-          <option value="The Moon">The Moon</option>
-        </select>
-      </div>
+
+
+    
     </div>
-    <div class="form-row">
-      <div class="form-col">
-        <label for="dino-color">Dino Colour</label>
-      </div>
-      <div class="form-col">
-        <select class="" v-model="color">
-          <option value="">What colour is your Dino?</option>
-          <option value="Red">Red</option>
-          <option value="Green">Green</option>
-          <option value="Blue">Blue</option>
-          <option value="Yellow">Yellow</option>
-        </select>
-      </div>
+
+      
     </div>
-    <div class="form-row">
-      <div class="form-col">
-        <h2>Can your Dino fly?</h2>
-      </div>
-      <div class="form-col">
-          <label for="dino-fly">Yes</label>
-          <input type="radio" id = "yes" name="fly" value="fly" v-model="picked">
-      </div>
-    </div>
-    <div class="form-row">
-      <label for="dino-fly">No</label>
-      <input type="radio" id ="no" name="fly" value="not fly" v-model="picked">
-    </div>
-    <h2>Is your Dino bigger than a house?</h2>
-    <label for="dino-house">Yes</label>
-    <input type="radio" id = "yes" name="house" value="bigger" v-model="selected">
-    <label for="dino-house">No</label>
-    <input type="radio" id ="no" name="house" value="smaller" v-model="selected">
+
+
+    
   </form>
 
   <p>{{name}} is a {{color}} dinosaur.
