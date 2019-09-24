3296f1c8c3d13c7074bd2404fe319f68d56350b8
Alex Woods <alex.woods89@hotmail.co.uk>
2019-09-24 09:42:05 +0100

    running

--- client/src/components/DinoDetail.vue
+++ client/src/components/DinoDetail.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
       <h3>{{dinosaur.name}}</h3>
-      <li v-if="dinosaur.eats">{{dinosaur.eats}}</li>
-      <li>{{dinosaur.period}}</li>
-      <li>{{dinosaur.regions[0]}}</li>
+      <li v-if="dinosaur.eats">Diet: {{dinosaur.eats}}</li>
+      <li>Period: {{dinosaur.period}}</li>
+      <li>Region: {{dinosaur.regions[0]}}</li>
       <img :src="dinosaur.pics[0].url" height="250">
       <br>
       <button v-on:click="addFavourite">Add to Favourites</button>
